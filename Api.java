04f9a4befcd51c7b8d9c525f80c992039261be5f
Gavin Yang <redppl@hotmail.com>
2016-10-30 11:15:22 +0800

    async version

--- Api.java
+++ Api.java
@@ -3,6 +3,17 @@ import java.util.List;
 import android.net.Uri;
 
 public interface Api {
-    List<Cat> queryCats(String query);
-    Uri store(Cat cat);
+    interface CatsQueryCallback {
+        void onCatListReceived(List<Cat> cats);
+        void onQueryFailed(Exception e);
+    }
+
+    interface StoreCallback{
+        void onCatStored(Uri uri);
+        void onStoreFailed(Exception e);
+    }
+
+    void queryCats(String query, CatsQueryCallback catsQueryCallback);
+
+    void store(Cat cat, StoreCallback storeCallback);
 }
