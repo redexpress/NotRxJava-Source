aab8f316daafbfd5236c34769195be2ca5903efe
Gavin Yang <redppl@hotmail.com>
2016-10-30 11:01:48 +0800

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,9 @@
+public class Cat implements Comparable<Cat>{
+    Bitmap image;
+    int cuteness;
+
+    @Override
+    public int compareTo(Cat another) {
+        return Integer.compare(cuteness, another.cuteness);
+    }
+}
