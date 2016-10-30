3da3ed880062ded0d2a2393faa07301d3c6a558d
Gavin Yang <redppl@hotmail.com>
2016-10-30 11:33:21 +0800

    Create ApiWrapper.java

---
+++ ApiWrapper.java
@@ -0,0 +1,35 @@
+import java.util.List;
+
+import android.net.Uri;
+
+public class ApiWrapper {
+    Api api;
+
+    public void queryCats(String query, final Callback<List<Cat>> catsCallback){
+        api.queryCats(query, new Api.CatsQueryCallback() {
+            @Override
+            public void onCatListReceived(List<Cat> cats) {
+                catsCallback.onResult(cats);
+            }
+
+            @Override
+            public void onQueryFailed(Exception e) {
+                catsCallback.onError(e);
+            }
+        });
+    }
+
+    public void store(Cat cat, final Callback<Uri> uriCallback){
+        api.store(cat, new Api.StoreCallback() {
+            @Override
+            public void onCatStored(Uri uri) {
+                uriCallback.onResult(uri);
+            }
+
+            @Override
+            public void onStoreFailed(Exception e) {
+                uriCallback.onError(e);
+            }
+        });
+    }
+}
