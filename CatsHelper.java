6a52e9e9467d70a059d7c4f8b6e3fe3846395320
Gavin Yang <redppl@hotmail.com>
2016-10-30 11:46:08 +0800

    work with AsyncJob

--- CatsHelper.java
+++ CatsHelper.java
@@ -3,23 +3,40 @@ import java.util.List;
 
 import android.net.Uri;
 
-public class CatsHelper{
+public class CatsHelper {
 
     ApiWrapper apiWrapper;
 
-    public void saveTheCutestCat(String query, final Callback<Uri> cutestCatCallback){
-        apiWrapper.queryCats(query, new Callback<List<Cat>>() {
+    public AsyncJob<Uri> saveTheCutestCat(final String query) {
+        return new AsyncJob<Uri>() {
             @Override
-            public void onResult(List<Cat> cats) {
-                Cat cutest = findCutest(cats);
-                apiWrapper.store(cutest, cutestCatCallback);
-            }
+            public void start(final Callback<Uri> cutestCatCallback) {
+                apiWrapper.queryCats(query)
+                        .start(new Callback<List<Cat>>() {
+                            @Override
+                            public void onResult(List<Cat> cats) {
+                                Cat cutest = findCutest(cats);
+                                apiWrapper.store(cutest)
+                                        .start(new Callback<Uri>() {
+                                            @Override
+                                            public void onResult(Uri result) {
+                                                cutestCatCallback.onResult(result);
+                                            }
 
-            @Override
-            public void onError(Exception e) {
-                cutestCatCallback.onError(e);
+                                            @Override
+                                            public void onError(Exception e) {
+                                                cutestCatCallback.onError(e);
+                                            }
+                                        });
+                            }
+
+                            @Override
+                            public void onError(Exception e) {
+                                cutestCatCallback.onError(e);
+                            }
+                        });
             }
-        });
+        };
     }
 
     private Cat findCutest(List<Cat> cats) {
