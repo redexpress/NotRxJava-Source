d75479a0fc67fecd8ff59a05d0f947b85c0cc0e0
Gavin Yang <redppl@hotmail.com>
2016-10-30 11:19:57 +0800

    async version

--- CatsHelper.java
+++ CatsHelper.java
@@ -5,13 +5,36 @@ import android.net.Uri;
 
 public class CatsHelper {
 
+    public interface CutestCatCallback {
+        void onCutestCatSaved(Uri uri);
+        void onError(Exception e);
+    }
+
     Api api;
 
-    public Uri saveTheCutestCat(String query){
-        List<Cat> cats = api.queryCats(query);
-        Cat cutest = findCutest(cats);
-        Uri savedUri = api.store(cutest);
-        return savedUri;
+    public void saveTheCutestCat(String query, final CutestCatCallback cutestCatCallback){
+        api.queryCats(query, new Api.CatsQueryCallback() {
+            @Override
+            public void onCatListReceived(List<Cat> cats) {
+                Cat cutest = findCutest(cats);
+                api.store(cutest, new Api.StoreCallback() {
+                    @Override
+                    public void onCatStored(Uri uri) {
+                        cutestCatCallback.onCutestCatSaved(uri);
+                    }
+
+                    @Override
+                    public void onStoreFailed(Exception e) {
+                        cutestCatCallback.onError(e);
+                    }
+                });
+            }
+
+            @Override
+            public void onQueryFailed(Exception e) {
+                cutestCatCallback.onError(e);
+            }
+        });
     }
 
     private Cat findCutest(List<Cat> cats) {
